33035f3b07be6fc6687c70fbd09d0723b4728546
Lovell Santimano <65031943+phoenix-pix@users.noreply.github.com>
2022-05-02 14:11:22 +0100

    added dependencies for files, updated text

--- weather.js
+++ weather.js
@@ -1,3 +1,8 @@
+require('dotenv').config()
+console.log(process.env) // remove this after you've confirmed it working
+
 let weather = {
-    
+    "apiKey": process.env
 }
+
+console.log(process.env);
